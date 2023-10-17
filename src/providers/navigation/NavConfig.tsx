2b5b2a1736885e6fa87b8d8289a25630a8fa6e03
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-10-17 16:38:45 +0100

    add font styling

--- src/providers/navigation/NavConfig.tsx
+++ src/providers/navigation/NavConfig.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 // import Login from "../../pages/login/Login";s
 import App from "../../App";
+import Main from "../../pages/Main";
 
 const NavConfig = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />}></Route>
+      <Route path="/" element={<App />}>
+        <Route path="/" element={<Main />} />
+      </Route>
     </Routes>
   );
 };
