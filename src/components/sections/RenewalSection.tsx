5d6c9d16f1b9a7c511171e2a0d72550ec9271acf
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-10-25 14:41:06 +0100

    changes renewal section

--- src/components/sections/RenewalSection.tsx
+++ src/components/sections/RenewalSection.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  getDriversCount,
+  getDriversWithExpiringDocuments,
+  getDriversWithLicenses,
+} from "../../api";
+import { MetricCard } from "../cards";
 
 const RenewalSection = () => {
-  return <div></div>;
+  const [noDrivers, setNoDrivers] = useState(0);
+  const [noValidDrivers, setNoValidDrivers] = useState(0);
+  const [noDriversExpiring, setNoDriversExpiring] = useState(0);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const driversCount = await getDriversCount();
+    const validDriversCount = await getDriversWithLicenses();
+    const driversExpiringCount = await getDriversWithExpiringDocuments();
+
+    setNoDrivers(driversCount);
+    setNoValidDrivers(validDriversCount);
+    setNoDriversExpiring(driversExpiringCount);
+  };
+
+  return (
+    <div className="Renewals_section">
+      <MetricCard title="Persons with drivers licenses" value={noDrivers} />
+      <MetricCard
+        title="Persons with valid drivers licenses"
+        value={noValidDrivers}
+      />
+      <MetricCard
+        title="Persons whose documents expire in 6 months"
+        value={noDriversExpiring}
+      />
+    </div>
+  );
 };
 
 export default RenewalSection;
