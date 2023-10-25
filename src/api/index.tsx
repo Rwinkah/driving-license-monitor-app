06625301bfdd678b5f467cbef48cdc6406a2ced9
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-10-25 09:10:50 +0100

    changes api - getDriversCount getDriversWithLicenses getDriversWithExpiringDocs

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,5 +1,5 @@
 import data from "./mockdata/drivers_data_final.json";
-import { Driver, DriverData } from "../types/drivers_data_interface";
+import { DriverData, Driver, Document } from "../types/drivers_data_interface";
 
 export const DriversApi = (page = 1, limit = 10): Promise<DriverData[]> => {
   return new Promise((resolve, reject) => {
@@ -27,4 +27,84 @@ export const DriversApi = (page = 1, limit = 10): Promise<DriverData[]> => {
       resolve(paginatedData as DriverData[]);
     }, 1000);
   });
+};
+
+export const getDriversCount = (): number => {
+  const driversData = data as DriverData[];
+  let count = 0;
+
+  driversData.forEach((driverData) => {
+    count += driverData.drivers.length;
+  });
+
+  return count;
+};
+
+export const getDriversWithLicenses = (): number => {
+  const driversData = data as DriverData[];
+  let count = 0;
+
+  driversData.forEach((driverData) => {
+    const drivers = driverData.drivers;
+    for (const driver of drivers) {
+      if (hasValidLicense(driver)) {
+        count++;
+      }
+    }
+  });
+
+  return count;
+};
+
+export const getDriversWithExpiringDocuments = (): number => {
+  const driversData = data as DriverData[];
+  let count = 0;
+
+  driversData.forEach((driverData) => {
+    const drivers = driverData.drivers;
+    for (const driver of drivers) {
+      for (const vehicle of driver.vehicles) {
+        for (const document of vehicle.documents) {
+          if (willExpireWithinSixMonths(document)) {
+            count++;
+            break; // If any document for a vehicle is expiring, count the person and exit the loop
+          }
+        }
+      }
+    }
+  });
+
+  return count;
+};
+
+// Helper function to check if a document will expire within the next 6 months
+const willExpireWithinSixMonths = (document: Document): boolean => {
+  if (document.expiresOn) {
+    const expirationDate = new Date(document.expiresOn);
+    const currentDate = new Date();
+
+    // Calculate the date 6 months from now
+    const sixMonthsFromNow = new Date();
+    sixMonthsFromNow.setMonth(currentDate.getMonth() + 6);
+
+    return expirationDate <= sixMonthsFromNow;
+  }
+
+  return false; // Handle the case where 'expiresOn' is missing or invalid
+};
+
+// Helper function to check if a driver has a valid license
+const hasValidLicense = (driver: Driver): boolean => {
+  const license = driver.driverLicense;
+  return (
+    license &&
+    license.status === "Valid" && // You can adjust this condition as needed
+    license.expiresOn >= getCurrentDate() // Assuming 'expiresOn' is a date string
+  );
+};
+
+// Helper function to get the current date in a format that can be compared with 'expiresOn'
+const getCurrentDate = (): string => {
+  const currentDate = new Date();
+  return currentDate.toISOString().split("T")[0]; // Format: YYYY-MM-DD
 };
