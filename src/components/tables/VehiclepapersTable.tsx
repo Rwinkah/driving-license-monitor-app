bffc9be635b6edc4a894e93f8f060f3b1b86757f
rwinkah <rwinosa@gmail.com>
2023-10-29 10:52:41 +0100

    finished styling vehiclepapers, driverslicense

--- src/components/tables/VehiclepapersTable.tsx
+++ src/components/tables/VehiclepapersTable.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { DriversApi } from "../../api";
 import { Driver, DriverData } from "../../types/drivers_data_interface";
+import '../../styles/VehiclepapersTable.scss'
 
 const VehiclePapersTable = () => {
   const [getDrivers, setDrivers] = useState<Driver[]>([]);
@@ -35,40 +36,43 @@ const VehiclePapersTable = () => {
       </div>
       <div className="tablebody_table">
         <div className="tablebody_table_header"></div>
-        <table>
+        <table className="maintable">
           <thead>
             <tr className="_firstrow">
-              <th></th>
+              <th className="_checkbox"></th>
               <th className="_name">Full name</th>
-              <th className="_docno">Phone number</th>
-              <th>Plate number</th>
-              <th>State</th>
-              <th>Class</th>
-              <th>Valid documents</th>
-              <th>Expired documents </th>
+              <th className="_phoneno">Phone number</th>
+              <th className="_plateno">Plate number</th>
+              <th className="_state">State</th>
+              <th className="_class">Class</th>
+              <th className="_valid">Valid documents</th>
+              <th className="_expire">Expired documents </th>
             </tr>
           </thead>
           <tbody>
             {getDrivers.map((driver) => (
               <tr key={driver.id}>
-                <td></td>
-                <td>{driver.fullName}</td>
-                <td>{driver.phoneNumber}</td>
-                <td>{driver.vehicles[0].class}</td>
-                <td>{driver.vehicles[0].plateNumber}</td>
-                <td>{driver.driverLicense.state}</td>
-                <td>{driver.vehicles[0].class}</td>
-                <td>{driver.vehicles[0].documents[0].documentName}</td>
-                <td>{driver.vehicles[0].documents[0].expiresOn}</td>
+                <td className="_checkbox"></td>
+                <td className="_drivername">{driver.fullName}</td>
+                <td className="_phoneno">{driver.phoneNumber}</td>
+                <td className="_plateno">{driver.vehicles[0].plateNumber}</td>
+                <td className="_state">{driver.driverLicense.state}</td>
+                <td className="_class">{driver.vehicles[0].class}</td>
+                <td className="_valid">
+                  {driver.vehicles[0].documents[0].documentName}
+                </td>
+                <td className="_expiredate">
+                  {driver.vehicles[0].documents[0].expiresOn}
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
         <div className="tablebody_table_bottom">
-          <div>
-            <p>{`Page ${page} of 1,234`}</p>
+          <div className="paging vehiclepagenum">
+            <p className="pagenum">{`Page ${page} of 1,234`}</p>
           </div>
-          <div>
+          <div className="paging"> 
             <button
               onClick={() => {
                 if (page === 1) return;
